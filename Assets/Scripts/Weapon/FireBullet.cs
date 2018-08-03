597d68613bbbbcbc465a7ff664587bf8061168ee
susenoo <36212651+susenoo@users.noreply.github.com>
2018-08-03 15:50:29 -0700

    update
    1. Created fire and shock gun/bullet
    2. Able to switch guns with 1,2,3 keys and scroll wheel

---
+++ Assets/Scripts/Weapon/FireBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour {
+
+    public float LifeTime;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Destroy(gameObject, LifeTime * Time.deltaTime);
+
+
+
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Weapon/ShockGun.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockGun : MonoBehaviour {
+
+    public GameObject ShockBulletPrefab;
+
+    public Transform ShockBulletSpawn;
+
+    public float ShockBulletSpeed = 30f;
+
+    public float lifeTime = 3f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject IceBullet = Instantiate(ShockBulletPrefab);
+
+        //Physics.IgnoreCollision(IceBullet.GetComponent<Collider>(),IceBulletSpawn.parent.GetComponent<Collider>());
+
+        IceBullet.transform.position = ShockBulletSpawn.position;
+
+        Vector3 rotation = IceBullet.transform.rotation.eulerAngles;
+
+        IceBullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+
+        IceBullet.GetComponent<Rigidbody>().AddForce(ShockBulletSpawn.forward * ShockBulletSpeed, ForceMode.Impulse);
+
+        StartCoroutine(DestoryIceBulletAfterTime(IceBullet, lifeTime));
+    }
+
+
+    private IEnumerator DestoryIceBulletAfterTime(GameObject IceBullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+    }
+}
