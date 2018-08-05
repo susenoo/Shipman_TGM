25e5da92a14d7deda65e4130657174f10f21e60f
susenoo <36212651+susenoo@users.noreply.github.com>
2018-08-05 09:44:56 -0700

    ui

---
+++ Assets/Scripts/Enemy/EnemyHealth1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth1 : MonoBehaviour {
+
+    public float = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(float damage)
+        if (HealthBar &lt;=0)
+    {
+        Destroy(GameObject);
+}
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public Image currentHealthBar;
+    public Text ratioText;
+
+    private float hitPoint = 100;
+    private float maxHitPoint = 100;
+
+	// Use this for initialization
+	void Start () {
+        UpdateHealthBar();
+
+    }
+
+    private void UpdateHealthBar()
+    {
+        float ratio = hitPoint / maxHitPoint;
+        currentHealthBar.rectTransform.localScale = new Vector3(ratio,1,1);
+        ratioText.text = (ratio*100).ToString() + '%';
+
+    }
+
+    private void TakeDamage(float damage)
+    {
+        hitPoint -= damage;
+        if (hitPoint < 0)
+            Debug.Log("Dead");
+        UpdateHealthBar();
+    }
+
+    private void HealDamage(float heal)
+    {
+        hitPoint += heal;
+        if (hitPoint > maxHitPoint)
+            hitPoint = maxHitPoint;
+        UpdateHealthBar();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
